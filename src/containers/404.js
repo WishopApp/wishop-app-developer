b01ef26f17dc1f1ae550880651e6b40df6b3186c
chanonpingpong <chanon.pingpong@gmail.com>
2018-09-26 01:03:18 +0700

    test env at netlify

--- src/containers/404.js
+++ src/containers/404.js
@@ -1,8 +1,11 @@
 import React from 'react'
-//
+import { Link } from 'react-static'
 
 export default () => (
   <div>
-    <h1>404 - Oh no's! We couldn't find that page :(</h1>
+    <h1>404 Page not found</h1>
+    <Link to="/">
+      <h3>home</h3>
+    </Link>
   </div>
 )
